4889cec8780185202676f749dd19de82c2f1e2a0
Seth Davis <setholito@gmail.com>
2018-08-19 20:39:01 -0500

    Make crumb use router link

--- src/ch-components/components/Crumb.vue
+++ src/ch-components/components/Crumb.vue
@@ -1,30 +1,30 @@
 <template>
     <li>
-        <a :href="url">
+        <router-link :to="url">
             <span
                 class="icon is-small"
                 v-if="iconName"
             >
                 <i :class="`fa fa-${iconName}`"></i>
             </span>
-            <slot></slot>
-        </a>
+            {{ text }}
+        </router-link>
     </li>
 </template>
 
 <script>
 export default {
     props: {
-        url: {
-            type: String,
-            default: '#'
+        iconName: {
+            type: String
         },
         text: {
             type: String,
             default: 'Link'
         },
-        iconName: {
-            type: String
+        url: {
+            type: String,
+            default: '/'
         }
     }
 }
